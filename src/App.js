c1d16f8c235d7194fe58600eb829b795fabb75ed
Kagan Degirmen <kagandegirmen@gmail.com>
2023-01-30 23:06:20 +0300

    Hero implementation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Hero />
     </div>
   );
 }
