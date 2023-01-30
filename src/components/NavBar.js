741befff52d469a1c08e91059736a6b855f24e80
Kagan Degirmen <kagandegirmen@gmail.com>
2023-01-30 22:35:31 +0300

    NavBar implementation

---
+++ src/components/NavBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <nav>
+      <img className="nav--logo" src={require("../images/airbnb-logo.png")} />
+    </nav>
+  );
+}
